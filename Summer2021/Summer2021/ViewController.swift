534f039ae5d4eafde3a3203f580139d8575b6fd4
Michael Ellison <mcellison3131@gmail.com>
2021-06-05 14:17:24 -0400

    test

--- Summer2021/Summer2021/ViewController.swift
+++ Summer2021/Summer2021/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TestLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+   
 }
 
