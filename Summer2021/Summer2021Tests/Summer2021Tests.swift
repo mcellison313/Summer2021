baf0316a872954ca24e1141ec4a845b89eab3a96
Michael Ellison <mcellison3131@gmail.com>
2021-06-05 13:14:44 -0400

    New App

---
+++ Summer2021/Summer2021Tests/Summer2021Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Summer2021Tests.swift
+//  Summer2021Tests
+//
+//  Created by Michael Ellison on 6/5/21.
+//
+
+import XCTest
+@testable import Summer2021
+
+class Summer2021Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
